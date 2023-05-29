9c138a55c72e91dcda633e17e50b38fbf2148386
jeka <epeunkov@gmail.com>
2023-05-29 19:42:48 +0300

    add custom view

--- app/src/main/java/com/example/myprofilelayouts/customview/MyButtonView.kt
+++ app/src/main/java/com/example/myprofilelayouts/customview/MyButtonView.kt
@@ -4,34 +4,36 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageView
 import androidx.core.content.res.ResourcesCompat
 import com.example.myprofilelayouts.R
 
 class MyButtonView(context: Context, attributes: AttributeSet? = null) : View(context, attributes) {
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
-    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.white_gray, null)
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.white_grey_light, null)
+    //override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        //super.onSizeChanged(width, height, oldWidth, oldHeight)
+        //if (::extraBitmap.isInitialized) extraBitmap.recycle()
 
-    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
-        super.onSizeChanged(width, height, oldWidth, oldHeight)
-        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        //extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        //extraCanvas = Canvas(extraBitmap)
+        //extraCanvas.drawColor(backgroundColor)
+    //}
 
-        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        extraCanvas = Canvas(extraBitmap)
-        extraCanvas.drawColor(backgroundColor)
-    }
-
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
-    }
+    //override fun onDraw(canvas: Canvas) {
+        //super.onDraw(canvas)
+        //canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+    //}
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        if (event.action == MotionEvent.ACTION_DOWN) {}
+        if (event.action == MotionEvent.ACTION_DOWN) {
+        }
         return true
     }
 }
